55e903630a458c87600fcb285873d859cffe44a7
Siphelelele Dilima <SipheleleDilima@hotmail.com>
2025-12-09 13:29:14 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {
   WalletProvider,
 } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter, } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
